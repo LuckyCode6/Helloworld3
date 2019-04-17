66f4851c1b81ccddd40e6b75faa1c1f4540eb0a8
yinxz88622 <yin_xz88622@163.com>
2019-04-17 23:23:30 +0800

    practise volatile

---
+++ src/VisibilityDemo.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.TimeUnit;
+
+public class VisibilityDemo {
+
+    private  static  volatile boolean is = true;
+
+    public static void main(String args[]){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                while(VisibilityDemo.is){
+
+//                    synchronized (this){
+                        i++;
+//                    }
+                }
+                System.out.println(i);
+            }
+        }).start();
+
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        VisibilityDemo.is = false;
+        System.out.println("被置为false了");
+    }
+
+}
